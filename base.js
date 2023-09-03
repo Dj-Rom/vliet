b835b63c9c977418ae8e76fd156d506c1bbb3f1b
Aleh Kapusta <118632628+Dj-Rom@users.noreply.github.com>
2023-09-03 16:15:54 +0200

    Add files via upload

---
+++ base.js
@@ -0,0 +1,81 @@
+export const baseClientNL = [
+    {name:"FLOWERPORTAL Naaldwijk",
+    adress: "Middel Broekweg 29, Honselersdijk",
+    gps:"https://www.google.com/maps/@52.0006422,4.2285385,16z?entry=ttu",
+    gpsCode: "52.00068843662913, 4.228474126987356",
+    passwordToDoor: "8244f"
+},
+{name:"WBE Naaldwijk",
+    adress: "Middel Broekweg 29, Honselersdijk",
+    gps:"https://goo.gl/maps/VonfsTy7hztRVAjQ7",
+    gpsCode: "52.00192925271974, 4.2294066331673505",
+    passwordToDoor: "*91727"
+},
+{name:"FLOWERPORTAL Aalsmeer",
+adress: "Legmeerdijk 313, Aalsmeer",
+gps:"https://goo.gl/maps/4c9a6vGKhHBAJurdA",
+gpsCode: "52.26156628333316, 4.774925119037935",
+passwordToDoor: "1010"
+},
+{name:"Oldenburger Aalsmeer",
+    adress: "Legmeerdijk 313, Aalsmeer",
+    gps:"https://goo.gl/maps/G3QSa1xSRsWKTHFY6",
+    gpsCode: "52.25784693921389, 4.7833214705594775",
+    passwordToDoor: "2094"
+},
+{name:"GreenConnect",
+adress: "unknow",
+gps:"unknow",
+gpsCode: "unknow",
+passwordToDoor: "1657b"
+},
+{name:"Geest",
+adress: "unknow",
+gps:"unknow",
+gpsCode: "unknow",
+passwordToDoor: "1488"
+},
+{name:"GreenConnect",
+adress: "Naaldwijk",
+gps:"unknow",
+gpsCode: "unknow",
+passwordToDoor: "1657b"
+},
+{name:"GreenConnect",
+adress: "Aalsmeer",
+gps:"unknow",
+gpsCode: "unknow",
+passwordToDoor: "1578"
+},
+,{name:"linsen",
+adress: "unknow",
+gps:"unknow",
+gpsCode: "unknow",
+passwordToDoor: "5926"
+},
+{name:"Fleura",
+adress: "unknow",
+gps:"unknow",
+gpsCode: "unknow",
+passwordToDoor: "4725"
+},
+{name:"Gerja",
+adress: "unknow",
+gps:"unknow",
+gpsCode: "unknow",
+passwordToDoor: "#2555"
+},
+{name:"Adomex",
+adress: "Aalsmeer",
+gps:"unknow",
+gpsCode: "unknow",
+passwordToDoor: "45622#"
+},
+{name:"Vliet",
+adress: "Aalsmeer",
+gps:"unknow",
+gpsCode: "unknow",
+passwordToDoor: "1428"
+},
+]
+ 
---
+++ main.js
@@ -0,0 +1,27 @@
+"use strict";
+const tableForListClient = document.querySelector(".list_client_NL");
+import { baseClientNL } from "./base.js";
+createHtmlTable(baseClientNL);
+
+function createHtmlTable(base) {
+  for (let i = 0; i < base.length; i++) {
+    let newTableAlTR = document.createElement("tr");
+    let tdName = document.createElement("td");
+    let tdAdress = document.createElement("td");
+    let tdGps = document.createElement("td");
+    let aTdGps = document.createElement("a");
+    let tdPass = document.createElement("td");
+    tdName.innerHTML = base[i].name;
+    tdAdress.innerHTML = base[i].adress;
+
+    aTdGps.innerHTML = base[i].gpsCode;
+    aTdGps.href = base[i].gps;
+    tdPass.innerHTML = base[i].passwordToDoor;
+    tdGps.appendChild(aTdGps);
+    newTableAlTR.appendChild(tdName);
+    newTableAlTR.appendChild(tdAdress);
+    newTableAlTR.appendChild(tdGps);
+    newTableAlTR.appendChild(tdPass);
+    tableForListClient.appendChild(newTableAlTR);
+  }
+}
