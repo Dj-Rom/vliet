2e5c0159682b08f1225b50f3b0b1347fe878b92e
Aleh Kapusta <118632628+Dj-Rom@users.noreply.github.com>
2023-09-03 20:24:20 +0200

    Update base.js

--- base.js
+++ base.js
@@ -23,12 +23,6 @@ passwordToDoor: "1010"
     gpsCode: "52.25784693921389, 4.7833214705594775",
     passwordToDoor: "2094"
 },
-{name:"GreenConnect",
-adress: "unknow",
-gps:"unknow",
-gpsCode: "unknow",
-passwordToDoor: "1657b"
-},
 {name:"Geest",
 adress: "unknow",
 gps:"unknow",
@@ -47,7 +41,7 @@ gps:"unknow",
 gpsCode: "unknow",
 passwordToDoor: "1578"
 },
-,{name:"linsen",
+,{name:"Linsen",
 adress: "unknow",
 gps:"unknow",
 gpsCode: "unknow",
@@ -77,5 +71,73 @@ gps:"unknow",
 gpsCode: "unknow",
 passwordToDoor: "1428"
 },
+{name:"Kuehne + Nagel Shiphol",
+adress: "Airbuslaan 2-5,  Schiphol",
+gps:"https://goo.gl/maps/o3ynVyNtpznENWdo8",
+gpsCode: "52.2891368227341, 4.773019530593184",
+passwordToDoor: "no"
+},
+{name:"IPHandlers",
+adress: "Noordpolderweg 20, Aalsmeer",
+gps:"https://goo.gl/maps/iSxSZbKVvyMFroJ26",
+gpsCode: "52.292871758069765, 4.807278636903448",
+passwordToDoor: "no"
+},
+{name:"Flora United International BV",
+adress: "Mijnsherenweg 4, De Kwakel",
+gps:"https://goo.gl/maps/xTMo89XCB7BhMKFv5",
+gpsCode: "52.2466758312792, 4.771200651967386",
+passwordToDoor: "unknow"
+},
+{name:"Marjoland B.V.",
+adress: "Bredeweg 1A, Waddinxveen",
+gps:"https://goo.gl/maps/KTf3jnpc6pzE9TBp9",
+gpsCode: "52.03636734195816, 4.59324833676838",
+passwordToDoor: "no"
+},
+{name:"Bonevo",
+adress: "Schoutenbosweg 6, Heerhugowaard",
+gps:"https://goo.gl/maps/KS7uw7MX4iK6onLJ6",
+gpsCode: "52.71000313566775, 4.839007519975328",
+passwordToDoor: "unknow"
+},
+{name:"Jupiter 159 Wematrans",
+adress: "Jupiter159, Honselersdijk",
+gps:"https://goo.gl/maps/REfVDh47wfppGW5W8",
+gpsCode: "51.99483 4.23572",
+passwordToDoor: "unknow"
+},
+{name:"Mans Flowers",
+adress: "Liesveldsesteeg 28, Brakel",
+gps:"https://goo.gl/maps/DQEgx54HpjRJu2AMA",
+gpsCode: "51.81365071657046, 5.112250232640258",
+passwordToDoor: "unknow"
+},
+{name:"Van Helvoort Company Brakel",
+adress: "Molenkampsweg 15b, Brakel",
+gps:"https://goo.gl/maps/GfdDPT7o78prTypt9",
+gpsCode: "51.81196804768243, 5.104947285881199",
+passwordToDoor: "unknow"
+},
+{name:"VHC Poederoijen",
+adress: "Poederoijen",
+gps:"https://goo.gl/maps/m61wNh8C84H2mnDT8",
+gpsCode: "51.79410729690693, 5.081173350936307",
+passwordToDoor: "unknow"
+},
+{name:"Wijk",
+adress: "Maarten van Rossumweg 2b, Poederoijen",
+gps:"https://goo.gl/maps/b8dJBiAWCMxnVmsv5",
+gpsCode: "51.78969489898063, 5.074589317455967",
+passwordToDoor: "unknow"
+},
+{name:"G & G Flowers B.V.",
+adress: "Ouwelsestraat 27, Gameren",
+gps:"https://goo.gl/maps/PNjDYm7AGBBJ1sSKA",
+gpsCode: "51.79965930399761, 5.212235624513583",
+passwordToDoor: "unknow"
+},
+
 ]
+ 
  
